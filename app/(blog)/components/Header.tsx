424c457034c7ded1a5461572e0abe503b6300a7b
Hilary liu <2788370451@qq.com>
2024-12-06 23:36:15 +0800

    添加blog首页路由active样式

--- app/(blog)/components/Header.tsx
+++ app/(blog)/components/Header.tsx
@@ -1,18 +1,34 @@
+"use client"
 import { Logo } from "@/app/components/Logo";
 import { ToggleTheme } from "@/app/components/ToggleTheme";
+import _ from "lodash";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import { FC } from "react";
 
+const blogNavs = [
+    {
+        name: 'Home',
+        href: '/blog'
+    },
+    {
+        name: 'Archives',
+        href: '/blog/archives'
+    }
+]
+
 export const Header: FC = () => {
+    const pathname = usePathname()
     return <div className="flex flex-row justify-between items-center h-10 mt-2 gap-2 pr-4">
         <Logo />
         <div className="flex justify-center items-center gap-4">
-            <Link href={`/blog`} className="btn">
-                Home
-            </Link>
-            <Link href={`/blog/archives`} className="btn">
-                Archives
-            </Link>
+            {
+                _.map(blogNavs, (n) => (
+                    <Link key={n.href} href={n.href} className={`btn ${pathname === n.href ? 'border-foreground' : ''}`}>
+                        {n.name}
+                    </Link>
+                ))
+            }
             <ToggleTheme />
         </div>
     </div>
