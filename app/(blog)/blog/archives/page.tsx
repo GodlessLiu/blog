333e4b6664473c2f74bfc8aa33011fa03810c528
Hilary liu <2788370451@qq.com>
2024-12-07 01:18:46 +0800

    添加force-dynamic

--- app/(blog)/blog/archives/page.tsx
+++ app/(blog)/blog/archives/page.tsx
@@ -3,11 +3,13 @@ import _ from "lodash";
 import { FC } from "react";
 import Archiveitem from "./components/Archiveitem";
 
+export const dynamic = "force-dynamic";
+
 const Articles: FC = async () => {
-    const data = await fetch("http://localhost:3000/api/bff/notion", {
+    const data = await fetch("http://127.0.0.1:3000/api/bff/notion", {
         method: "GET"
     })
-    const { data: archives } = (await data.json()) as { data: ArchiveItemPost[] };
+    const { data: archives = [] } = (await data.json()) as { data: ArchiveItemPost[] };
     return <div className="pt-20">
         {
             _.map(archives, a => (
--- app/api/bff/notion/route.ts
+++ app/api/bff/notion/route.ts
@@ -1,7 +1,7 @@
 import { queryDataBase2ArchiveItems } from "@/app/utils/formatter"
 import {Client} from "@notionhq/client"
 
-export const revalidate = 60
+// export const revalidate = 60
 
 const notion = new Client({
     auth: process.env.NOTION_API_TOKEN
@@ -9,16 +9,23 @@ const notion = new Client({
 
 // 获取所有文章 TODO: 分页[加载更多]
 export async function GET() {
-    const data = (await notion.databases.query({
-        database_id: process.env.PAGE_ID!,
-        sorts: [
-            {
-                "timestamp": "created_time",
-                "direction": "descending"
-            }
-        ]
-    }))
-    return Response.json({
-        data:queryDataBase2ArchiveItems(data.results)
-    })
+    try{
+        const data = (await notion.databases.query({
+            database_id: process.env.PAGE_ID!,
+            sorts: [
+                {
+                    "timestamp": "created_time",
+                    "direction": "descending"
+                }
+            ]
+        }))
+        return Response.json({
+            data:queryDataBase2ArchiveItems(data.results)
+        })
+    }catch (e){
+        console.log(e)
+        return Response.json({
+            data:null
+        })
+    }
 }
