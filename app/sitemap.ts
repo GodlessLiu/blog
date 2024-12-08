30aa3b2316e03c11f0d8f546e57d13b01f5e42c0
Hilary liu <2788370451@qq.com>
2024-12-08 18:01:53 +0800

    修改fetch报错方式

---
+++ app/sitemap.ts
@@ -0,0 +1,17 @@
+import type { MetadataRoute } from "next";
+import { fetchData } from "./utils/fetch";
+import { ArchiveItemPost } from "./types/blog/archives";
+import _ from "lodash";
+
+export default async  function sitemap(): Promise<MetadataRoute.Sitemap> {
+    const {data:archives} = await fetchData("/api/bff/notion/archives",{method:"GET"}) as {data:ArchiveItemPost[]}
+    if(!archives) return []
+    return _.map(archives,(d)=>{
+        return {
+            url:process.env.DOMAIN + `/blog/post/${d.id}`,
+            lastModified: d.last_edited_time,
+            changeFrequency: "daily",
+            priority: 0.7
+        }
+    })
+}
--- app/utils/fetch.ts
+++ app/utils/fetch.ts
@@ -4,6 +4,6 @@ export async function fetchData<T>(url: string,options?: RequestInit): Promise<T
     .then((data) => data as T)
     .catch((error) => {
       console.error('Error fetching data:', error);
-      throw error;
+      return [] as T;
     });
 }
